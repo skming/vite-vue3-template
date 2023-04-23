dd08cff56ba4816fe46abc827072e32b2c8f72cf
shikm <shikm@senlianshop.com>
2023-04-23 15:44:26 +0800

    chore: release version

--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -1,13 +1,17 @@
 <script lang="ts" setup>
+import { testStore } from '@/store/modules'
 
+const test = testStore()
 </script>
 
 <template>
   <div class="text-24px text-green">
-    测试页面
+    测试页面<a class="text">{{ test.name }}</a>
   </div>
 </template>
 
 <style lang="scss" scoped>
-
+.text {
+  @apply text-coolGray underline cursor-pointer hover:text-blue-6;
+}
 </style>
