12fd43550f75d27de7bc54fae78bc43a53010493
skming <shikm@senlianshop.com>
2026-04-21 14:53:26 +0800

    chore: update axios type definitions to include custom response properties

--- src/typings/axios.d.ts
+++ src/typings/axios.d.ts
@@ -1,9 +1,9 @@
 declare module 'axios' {
-  export interface AxiosRequestConfig {
+  interface AxiosRequestConfig {
     repeatCancel?: boolean // 是否开启重复请求取消
     loading?: boolean // 是否显示加载提示
   }
-  export interface InternalAxiosRequestConfig {
+  interface InternalAxiosRequestConfig {
     repeatCancel?: boolean
     loading?: boolean
   }
